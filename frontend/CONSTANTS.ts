91a7d4d0b74b0c9496bcd3d67394c5f198f4efb3
Eugene-Stone <eugenestone.work@gmail.com>
2026-07-13 14:10:17 +0300

    Constants new

--- frontend/CONSTANTS.ts
+++ frontend/CONSTANTS.ts
@@ -1,4 +1,5 @@
 export const FRONTEND_URL = 'http://localhost:5173/NordWood';
 // export const FRONTEND_URL = 'https://eugene-stone.github.io/NordWood/';
 
-export const BACKEND_URL = 'http://localhost:1337';
+// export const BACKEND_URL = 'http://localhost:1337';
+export const BACKEND_URL = 'https://nordwood.onrender.com';
